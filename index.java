b8d8b2aadb534ba4fa67aa32649d752e0a790620
Andrew Yahovskiy <mr.yahovskiy@gmail.com>
2019-10-06 19:31:58 +0300

    create issue

--- index.java
+++ index.java
@@ -2,7 +2,7 @@ public class Sitairis5
 {
 	public static void main(String args[])
 	{
+	String a = "this is string";
 	System.out.println("third state");
-	System.out.println("create new branch");
 	}
 }
