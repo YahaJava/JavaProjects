ff1ab2702c17989b63abc86cf392ba9a12eba891
Andrew Yahovskiy <mr.yahovskiy@gmail.com>
2019-10-07 18:03:57 +0300

    dfgdhdg

--- index.java
+++ index.java
@@ -2,7 +2,7 @@ public class Sitairis5
 {
 	public static void main(String args[])
 	{
-	String a = "this is a string";
+	String a = "this is string";
 	System.out.println("fourth state");
 	System.out.println("third state");
 	}
